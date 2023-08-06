d14a65fccd7ca015f5648642223f4d015cf3611d
Yusriiir <140380815+yusriiirrr19@users.noreply.github.com>
2023-08-06 20:00:36 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,69 @@
+<?php
+session_start();
+
+include("php/config.php");
+if (!isset($_SESSION['valid'])) {
+    header("Location: index.php");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style/style.css">
+</head>
+
+<body>
+    <div class="nav">
+        <div class="logo">
+            <p>Logo</p>
+        </div>
+
+        <div class="right-links">
+
+            <?php
+
+            $id = $_SESSION['id'];
+            $query = mysqli_query($con, "SELECT*FROM users WHERE Id=$id");
+
+            while ($result = mysqli_fetch_assoc($query)) {
+                $res_Uname = $result['Username'];
+                $res_Email = $result['Email'];
+                $res_Age = $result['Age'];
+                $res_Gender = $result['Gender'];
+                $res_id = $result['Id'];
+            }
+
+            echo "<a href='edit.php?Id=$res_id'>Change Profile</a>";
+            ?>
+
+            <a href="php/logout.php"> <button class="btn">Log Out</button> </a>
+
+        </div>
+    </div>
+    <main>
+
+        <div class="main-box top">
+            <div class="top">
+                <div class="box">
+                    <p>Hello <b><?php echo $res_Uname ?></b>, Welcome </p>
+                </div>
+                <div class="box">
+                    <p>Your email is <b> <?php echo $res_Email ?></b>.</p>
+                </div>
+            </div>
+            <div class="bottom">
+                <div class="box">
+                    <p>And you are <b><?php echp $res_Age ?></b>.</p>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
